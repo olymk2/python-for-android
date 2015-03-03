8eb6c3224d75378f03b6b3efb474c8ceffa0b4b2
Oliver Marks <oly@digitaloctave.com>
2015-03-03 22:56:13 +0000

    add echo

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
+#https://groups.google.com/forum/?hl=en#!searchin/python-android/ctypes/python-android/vDaaJXNYz_A/eazMVI-DQSYJ
 #/home/oly/.buildozer/android/platform/
+
+export ANDROIDSDK="/home/omarks/.buildozer/android/platform/android-sdk-20"
+export ANDROIDNDK="/home/omarks/.buildozer/android/platform/android-ndk-r9d"
+export ANDROIDNDKVER=r9
+export ANDROIDAPI=14
+
 export ANDROIDSDK="/home/oly/development/android-sdk-linux"
 export ANDROIDNDK="/home/oly/development/android-ndk-r7"
 export ANDROIDNDKVER=r7
