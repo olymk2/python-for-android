d5f8ff03b7b865bc950f4c48da8fd082dc78bec6
Oliver Marks <oly@digitaloctave.com>
2015-03-03 22:01:05 +0000

    wip

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
+#/home/oly/.buildozer/android/platform/
 export ANDROIDSDK="/home/oly/development/android-sdk-linux"
 export ANDROIDNDK="/home/oly/development/android-ndk-r7"
 export ANDROIDNDKVER=r7
 export ANDROIDAPI=14
 
 #./distribute.sh -f -m "pil"
-./distribute.sh -m "python" > /tmp/p4a-log.txt
+./distribute.sh -f -m "python" 
+#> /tmp/p4a-log.txt
